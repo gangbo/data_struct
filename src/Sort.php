a500206f59cae455ef8f89cecbd54d7d87357692
gangbo <daigangbo@gmail.com>
2017-06-06 19:11:50 +0800

    bubble sort

--- src/Sort.php
+++ src/Sort.php
@@ -1,14 +1,16 @@
 <?php
+
 namespace src;
 
 
 class Sort
 {
-    public static function foo()
-    {
-        return 5;
-    }
 
+    /**
+     * 快速排序
+     * @param array $intArray
+     * @return array
+     */
     public static function quick($intArray = [])
     {
         if (count($intArray) < 2) {
@@ -24,5 +26,57 @@ class Sort
         array_push($left, $first);
         $right = static::quick($right);
         return array_merge($left, $right);
+    }
+
+    /**
+     * 冒泡排序
+     * @param array $intArr
+     * @return array
+     */
+    public static function bubble($intArr = [])
+    {
+        $arrSize = count($intArr);
+        for ($i = 0; $i < $arrSize - 1; $i++) {
+            for ($j = 0; $j < $arrSize - 1 - $i; $j++) {
+                if ($intArr[$j] > $intArr[$j + 1]) {
+                    self::swap($intArr[$j], $intArr[$j + 1]);
+                }
+            }
+
+        }
+        return $intArr;
+    }
+
+    /**
+     * 交换两个数值
+     * @param $a
+     * @param $b
+     */
+    public static function swap(&$a, &$b)
+    {
+        $temp = $b;
+        $b = $a;
+        $a = $temp;
+    }
+
+    /**
+     * 字条串旋转, 如把abcdef前面两个字符ab移到字符串末尾
+     * @param array $array
+     * @param int $pos
+     * @return array
+     */
+    public static function reverseArr($array = [], $pos)
+    {
+        $reversedArr = [];
+        $leftArr = [];
+        foreach ($array as $k => $v) {
+            if ($k <= $pos) {
+                array_push($leftArr, $v);
+            } else {
+                array_push($reversedArr, $v);
+            }
+        }
+
+        return array_merge($reversedArr, $leftArr);
     }
 }
--- tests/SortTest.php
+++ tests/SortTest.php
@@ -30,4 +30,42 @@ class SortTest extends TestCase
         $this->assertTrue($expectedData === $sortedData);
     }
 
+    public function testReverseArr()
+    {
+        $originalArr = ['a', 'b', 'c', 'd', 'e', 'f'];
+        $expectedArr = ['c', 'd', 'e', 'f', 'a', 'b'];
+        $reversedArr = Sort::reverseArr($originalArr, 1);
+        $this->assertTrue($reversedArr === $expectedArr, '旋转数组失败');
+    }
+
+    public function testSwap()
+    {
+        $a = 5;
+        $b = 8;
+        Sort::swap($a, $b);
+        $this->assertEquals(8, $a, 'swap,交换失败 1');
+        $this->assertEquals(5, $b, 'swap,交换失败 2');
+
+    }
+
+    public function testBubble()
+    {
+        //case 1
+        $unSortedData = [3, 2, 1, 5];
+        $expectedData = [1, 2, 3, 5];
+        $sortedData = Sort::bubble($unSortedData);
+        $this->assertTrue($expectedData === $sortedData);
+        //case 2
+        $unSortedData = [10, 5, 18, 21, 9, 102, 59, 81, 100, 0, 31];
+        $expectedData = [0, 5, 9, 10, 18, 21, 31, 59, 81, 100, 102];
+        $sortedData = Sort::bubble($unSortedData);
+        $this->assertTrue($expectedData === $sortedData);
+        //case 1
+        $unSortedData = [2, 1];
+        $expectedData = [1, 2];
+        $sortedData = Sort::bubble($unSortedData);
+        $this->assertTrue($expectedData === $sortedData);
+
+    }
+
 }
