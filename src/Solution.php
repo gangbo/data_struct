29f1c13b6f181b9119b4e5388e27c155b422d08d
gangbo.dai <gangbo.dai@ifchange.com>
2019-12-30 21:37:03 +0800

    #

---
+++ src/Solution.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ *
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/21/
+ * Created by daigangbo.
+ * User: daigangbo <gangbo.dai@ifchange.com>
+ * Date: 2019/12/30
+ * Time: 21:22
+ */
+
+namespace src;
+
+
+class Solution
+{
+
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+
+    }
+
+}
---
+++ tests/SolutionTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by daigangbo.
+ * User: daigangbo <gangbo.dai@ifchange.com>
+ * Date: 2019/12/30
+ * Time: 21:24
+ */
+
+namespace tests;
+
+
+class SolutionTest
+{
+
+}
